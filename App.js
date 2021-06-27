64fcdc27829f73f68ae0ebaf9b1c43396f5c2c8b
Ly Tuong Thuy <thuylt@runsystem.net>
2021-06-27 19:24:40 +0700

    Create Animated Eye

--- App.js
+++ App.js
@@ -1,112 +1,165 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Animated,
   StyleSheet,
-  Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
+  Easing
 } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+function EyePart({ rotate = false }) {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <Svg
+      style={{
+        height: 150,
+        width: 300,
+        transform: [{ rotate: rotate ? '180deg' : '0deg' }]
+      }}
+    >
+      <Path d="M 0 150 Q 150 0 300 150 Q 150 150 0 150" fill="white" />
+    </Svg>
+  );
+}
+
+const App = () => {
+  const [translateX] = useState(new Animated.Value(0));
+  const [eyelidAnim] = useState(new Animated.Value(0));
+  const eyelidRef = useRef(null);
+
+  const getEyelidPath = value => {
+    return `M 0 150 Q 150 0 300 150 Q 150 ${value * 100} 0 150`;
+  };
+
+  useEffect(() => {
+    eyelidAnim.addListener(({ value }) => {
+      eyelidRef.current.setNativeProps({ d: getEyelidPath(value) });
+    });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <>
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={() => {
+            Animated.timing(translateX, {
+              toValue: 50,
+              duration: 600,
+              useNativeDriver: true
+            }).start(() => {
+              Animated.timing(translateX, {
+                toValue: -50,
+                duration: 1200,
+                useNativeDriver: true
+              }).start(() => {});
+            });
+
+            Animated.timing(eyelidAnim, {
+              toValue: 3,
+              duration: 200,
+              useNativeDriver: false,
+              easing: Easing.bezier(0.83, 0, 0.17, 1)
+            }).start(() => {
+              Animated.timing(eyelidAnim, {
+                toValue: 0.5,
+                duration: 200,
+                useNativeDriver: false,
+                easing: Easing.bezier(0.83, 0, 0.17, 1)
+              }).start();
+            });
+          }}
+        >
+          <View style={{ position: 'absolute', left: 50, top: -80 }}>
+            <Svg style={{ height: 150, width: 200 }}>
+              <Path
+                d="M 0 150 Q 100 0 200 150 Q 100 50 0 150"
+                fill="rgb(54,64,224)"
+              />
+            </Svg>
+          </View>
+
+          <EyePart />
+          <EyePart rotate={true} />
+
+          <View
+            style={{
+              position: 'absolute',
+              transform: [{ rotate: '180deg' }],
+              bottom: -80,
+              left: 50
+            }}
+          >
+            <Svg style={{ height: 150, width: 200 }}>
+              <Path
+                d="M 0 150 Q 100 0 200 150 Q 100 50 0 150"
+                fill="rgb(54,64,224)"
+              />
+            </Svg>
+          </View>
+
+          <Animated.View style={{ position: 'absolute', transform: [{ translateX }] }}>
+            <View style={styles.eyeContainer}>
+              <View style={styles.outerCircle} />
+            </View>
+            <View style={styles.eyeContainer}>
+              <View style={styles.innerCircle} />
+            </View>
+            <View style={styles.eyeContainer}>
+              <View style={styles.light} />
+            </View>
+          </Animated.View>
+
+          <View style={{ position: 'absolute' }}>
+            <Svg style={{ height: 300, width: 300 }}>
+              <Path
+                ref={ref => (eyelidRef.current = ref)}
+                d={getEyelidPath(1)}
+                fill="rgb(54,64,224)"
+              />
+            </Svg>
+          </View>
+        </TouchableOpacity>
+      </>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(34,110,231)',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  eyeContainer: {
+    position: 'absolute',
+    width: 300,
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 75
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  outerCircle: {
+    backgroundColor: 'rgb(48,169,246)',
+    height: 100,
+    width: 100,
+    borderRadius: 50
   },
-  highlight: {
-    fontWeight: '700',
+  innerCircle: {
+    backgroundColor: 'rgb(18,22,91)',
+    height: 70,
+    width: 70,
+    borderRadius: 35
   },
+  light: {
+    backgroundColor: 'white',
+    height: 15,
+    width: 15,
+    borderRadius: 7.5,
+    marginLeft: 20,
+    marginBottom: 20
+  }
 });
 
 export default App;
